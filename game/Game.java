5279473e9367498d705110f77cc24088aabe222a
Alexei Yurikov <roboshadow@mail.ru>
2022-07-30 20:34:29 +0300

    fixed class game

--- game/Game.java
+++ game/Game.java
@@ -21,33 +21,11 @@ public class Game {
             res = ap.getCandies(number);
             System.out.printf("AI choice - %d candies\n", res);
             number -= res;
-            System.out.printf("Total candies left - %d\n", number);
             return number;
 
         }
 
     }
-//    public void GameLoop(int number) {
-//        System.out.printf("Total candies left - %d\n", number);
-//        while (number > 0) {
-//            if (number == 2) {
-//                System.out.println("Game over!");
-//                return;
-//            } else {
-//                System.out.println("---Human move---");
-//                res = hp.getCandies(number);
-//                number -= res;
-//                System.out.printf("Total candies left - %d\n", number);
-//                System.out.println("---AI move---");
-//                res = ap.getCandies(number);
-//                System.out.printf("AI choice - %d candies\n", res);
-//                number -= res;
-//                System.out.printf("Total candies left - %d\n", number);
-////                sv.getSave(g);
-//
-//            }
-//        }
-//
-//    }
+
 
 }
